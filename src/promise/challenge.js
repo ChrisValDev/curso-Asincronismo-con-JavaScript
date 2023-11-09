e9bf02a14a8e28b24cb81b3fe477edc9532bf2ee
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-09 17:35:05 -0600

    FetchData with Promise

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -5,12 +5,30 @@ function fetchData(urlApi) {
     return fetch(urlApi);
 };
 
+// fetchData(`${API}/products`)
+//     .then(response => response.json())
+//     .then(products => {
+//         console.log(products);
+//     })
+//     .then(() => {
+//         console.log('hola')
+//     })
+//     .catch(error => console.log(error));
+
 fetchData(`${API}/products`)
     .then(response => response.json())
     .then(products => {
-        console.log(products);
+        console.log('products', products);
+        return fetchData(`${API}/products/${products[0].id}`)
     })
-    .then(() => {
-        console.log('hola')
+    .then(response => response.json())
+    .then(product => {
+        console.log('product', product.title);
+        return fetchData(`${API}/categories/${product.category.id}`)
     })
-    .catch(error => console.log(error));
+    .then(response => response.json())
+    .then(category => {
+        console.log('category', category.name);
+    })
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finally'));

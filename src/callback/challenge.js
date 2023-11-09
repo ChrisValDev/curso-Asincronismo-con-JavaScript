6d978aa8d4b3821a43974ce29b75c8eda1b6fe6e
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-09 14:21:16 -0600

    Fetch Data with callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest'); // XMLHttpRequest objeto con diversas propiedades.
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest; // XMLHttpRequest objeto con diversas propiedades.
 const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlApi, callback) {
@@ -9,11 +9,24 @@ function fetchData(urlApi, callback) {
         if (xhttp.readyState === 4) { // readyState 4 = Completed, hay 5 estados de 0 a 4.  
             if (xhttp.status === 200) { // status 200 - 299 = Respuestas satisfactorias, hay mas status
                 callback(null, JSON.parse(xhttp.responseText)); // Recibir lo que entrege el servidor en string como objeto.
+            } else {
+                const error = new Error('Error', urlApi);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error('Error', urlApi);
-            return callback(error, null);
         }
     }
     xhttp.send(); // Enviar
 }
+
+fetchData(`${API}/products`, function (error1, data1) {
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+            if (error3) return console.error(error3);
+            console.log('data1', data1[0]);
+            console.log('data2', data2.title);
+            console.log('data3', data3.name);
+        }) 
+    })    
+});

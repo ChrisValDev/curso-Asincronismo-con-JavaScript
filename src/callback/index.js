00f92e9363fff4e6ecccb4a3375348c049973138
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-09 12:21:11 -0600

    Example of setTimeout in JS

--- src/callback/index.js
+++ src/callback/index.js
@@ -7,3 +7,13 @@ function calc(num1, num2, callback) {
 };
 
 console.log(calc(5, 8, sum));
+
+setTimeout(function() {
+    console.log("Hola JavaScript con setTimeout");
+}, 4000)
+
+function greeting(name) {
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(greeting, 4000, 'Christopher');
